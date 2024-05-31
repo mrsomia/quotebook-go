c8871153f8c50db77ee80349d234204b326c4f0b
Sachin Somia <sachinsomia@gmail.com>
2024-05-31 14:35:50 +0100

    feat: handles post body for get quote

--- api.go
+++ api.go
@@ -3,45 +3,55 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"log"
+	"fmt"
 	"net/http"
 )
 
 type ApiServer struct {
-  svc Service
-  mux *http.ServeMux
+	svc Service
+	mux *http.ServeMux
 }
 
 func NewApiServer(svc Service) *ApiServer {
-  s := http.NewServeMux()
-  apiServer := &ApiServer{
-    svc: svc,
-    mux: s,
-  }
-  return apiServer
+	s := http.NewServeMux()
+	apiServer := &ApiServer{
+		svc: svc,
+		mux: s,
+	}
+	return apiServer
 }
 
 func (s *ApiServer) Start(addr string) error {
-  s.mux.HandleFunc("GET /quote", s.handleGetQuote)
-  server := &http.Server{
-    Addr: addr,
-    Handler: s.mux,
-  }
-  return server.ListenAndServe()
+	s.mux.HandleFunc("POST /quote", s.handleGetQuote)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: s.mux,
+	}
+	return server.ListenAndServe()
 }
 
 func (s *ApiServer) handleGetQuote(w http.ResponseWriter, r *http.Request) {
-  // TODO: read int from request
-  quote, err := s.svc.GetQuote(context.Background(), 1)
-  if err != nil {
-    writeJSON(w, http.StatusUnprocessableEntity, map[string]string{"error": err.Error()})
-    log.Fatal(err)
-  }
-  writeJSON(w, http.StatusOK, quote)
+	d := json.NewDecoder(r.Body)
+	b := struct {
+		ID int `json:"id"`
+	}{}
+
+	if err := d.Decode(&b); err != nil {
+		s := fmt.Sprintf("Unable to read JSON\nerror: %v", err.Error())
+		writeJSON(w, http.StatusUnprocessableEntity, map[string]string{"error": s})
+		return
+	}
+
+	quote, err := s.svc.GetQuote(context.Background(), b.ID)
+	if err != nil {
+		writeJSON(w, http.StatusUnprocessableEntity, map[string]string{"error": err.Error()})
+		return
+	}
+	writeJSON(w, http.StatusOK, quote)
 }
 
 func writeJSON(w http.ResponseWriter, s int, v any) error {
-  w.WriteHeader(s)
-  w.Header().Add("Content-Type", "application/json")
-  return json.NewEncoder(w).Encode(v)
+	w.WriteHeader(s)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
